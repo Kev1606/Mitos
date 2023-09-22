8bdb96d1fe065685c56c79be2cbdf31ec23f9cac
josejulian1604 <85717661+josejulian1604@users.noreply.github.com>
2023-09-21 19:47:49 -0600

    Se implementa un menu para que el usuario elija el programa que desea ejecutar

--- Mitos.py
+++ Mitos.py
@@ -3,36 +3,40 @@
 from cargarArchivo import cargarArchivo as func
 from ExploradorMitos import Explorador 
 
-import argparse
+# Funcion principal del explorador
+# Entradas: recibe la ruta del archivo que el usuario desea procesar
+# Salidas: Imprime la lista de componentes lexicos 
+def mitos(ruta):
+    texto = func.leer_archivo(ruta) # Llama a la funcion leer_archivo de la carpeta cargarArchivo
+    exp = Explorador(texto) # Invoca el constructor del explorador. Se encuentra en el archivo ExploradorMitos
+    exp.explorar()
+    exp.imprimir_componentes()
 
-# Se crea un objeto ArgumentParser que se utiliza para definir como se deben procesar los 
-# argumentos desde la linea de comandos
-parser = argparse.ArgumentParser(description='Explorador para el lenguaje Mitos')
-
-# En la siguiente linea, el comando '--solo-explorar' es un argumento el cual indica que solo
-# se desea ejecutar el explorador, ademas se agrega el comando --help para brindar al usuario
-# la informacion necesaria para la ejecucion del programa
-parser.add_argument('--solo-explorar', dest='explorador', action='store_true', 
-        help='ejecuta solo el explorador e imprime la lista de componentes léxicos')
-
-# Proporciona la ruta del archivo como entrada 
-parser.add_argument('archivo',
-        help='Archivo de código fuente')
-
-def mitos():
-
-    args = parser.parse_args() # Procesa los comandos desde linea de comandos
-
-    # En caso de que el usuario brinde como argumento --solo-explorar
-    if args.explorador is True: 
-        texto = func.leer_archivo(args.archivo) # Llama a la funcion cagar_archivo de la carpeta cargarArchivo
-        exp = Explorador(texto)
-        exp.explorar()
-        exp.imprimir_componentes()
-        
-    else:
-        parser.print_help()
-
+# Despliega un menu para ejecutar cualquiera de los 5 programas disponibles
+# Invoca al metodo mitos para el procesamiento del archivo
+# Entradas: Un numero del 1-5
+def menu():
+    while(True):
+        print('Ingrese el numero de archivo que desea ejecutar (presione enter para salir): ')
+        print("1. Fibonacci")
+        print("2. Factorial")
+        print("3. EsPrimo")
+        print("4. DiaSemana")
+        print("5. ConvertirABinario")
+        opcion = input("Ingrese el numero: ")
+        if opcion == '1':
+            mitos(".\Ejemplos\Fibonacci.txt")
+        elif opcion == '2':
+            mitos(".\Ejemplos\Factorial.txt")
+        elif opcion == '3':
+            mitos(".\Ejemplos\EsPrimo.txt")
+        elif opcion == '4':
+            mitos(".\Ejemplos\DiaSemana.txt")
+        elif opcion == '5':
+            mitos(".\Ejemplos\ConvertirABinario.txt")
+        else:
+            break
+        input("Presione Enter para continuar")
 
 if __name__ == '__main__':
-    mitos()
+    menu()
