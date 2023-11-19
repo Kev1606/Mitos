2eaf7e960e30f6a9ee13c9b71294f297d47b848d
AxelCHCR <axelchaves.r@estudiantec.cr>
2023-11-19 01:46:21 -0600

    feat: maqueta de las funciones del tipo de todo definidas

--- analizador/Generador/visitadores.py
+++ analizador/Generador/visitadores.py
@@ -4,3 +4,63 @@ class VisitantePython:
     tabuladores = 0
     def visitar(self, nodo: TipoNodo):
         resultado = ""
+    """ Aquí tendrán que ir las comparaciones basadas en lo 
+     que venga del verificador cuando estén listas."""
+    def visitarAsignacion(self, nodoActual):
+        return
+    def visitarExpresionMatematica(self, nodoActual):
+        return
+    def visitarExpresion(self, nodoActual):
+        return
+    def visitarFuncion(self, nodoActual):
+        return
+    def visitarInvocacion(self, nodoActual):
+        return
+    def visitarParametrosInvocacion(self, nodoActual):
+        return
+    def visitarParametrosFuncion(self, nodoActual):
+        return
+    def visitarInstruccion(self, nodoActual):
+        return
+    def visitarRepeticion(self, nodoActual):
+        return
+    def visitarBifurcacion(self, nodoActual):
+        return
+    def visitarTemis(self, nodoActual):
+        return
+    def visitarSino(self, nodoActual):
+        return
+    def visitarOperadorLogico(self, nodoActual):
+        return
+    def visitarCondicion(self, nodoActual):
+        return
+    def visitarComparacion(self, nodoActual):
+        return
+    def visitarRetorno(self, nodoActual):
+        return
+    def visitarError(self, nodoActual):
+        return
+    def visitarPrincipal(self, nodoActual):
+        return
+    def visitarBloqueInstrucciones(self, nodoActual):
+        return
+    def visitarOpeMate(self, nodoActual):
+        return
+    def visitarComparador(self, nodoActual):
+        return
+    def visitarVariable(self, nodoActual):
+        return
+    def visitarBooleano(self, nodoActual):
+        return
+    def visitarTexto(self, nodoActual):
+        return
+    def visitarEntero(self, nodoActual):
+        return 
+    def visitarFlotante(self, nodoActual):
+        return
+    def visitarIdentificador(self, nodoActual):
+        return
+    def visitarTipo(self, nodoActual):
+        return
+    def visitarString(self, nodoActual):
+        return
